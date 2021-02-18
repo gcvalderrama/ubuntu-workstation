432c3bd45a6dadb9ee4547fb1308cbefe7912552
Gregory Valderrama <gcvalderrama@hotmail.com>
2021-02-18 12:31:25 -0500

    add

---
+++ desktop.bash
@@ -0,0 +1,19 @@
+# install 
+
+# sudo apt install wmctrl
+
+
+wmctrl -r Opera -t 0
+wmctrl -r Telegram  -t 0
+wmctrl -r 'Mozilla Thunderbird'  -t 0
+wmctrl -r Google Chrome -t 1
+wmctrl -r Brave -t 2
+wmctrl -r f106 -t 2
+wmctrl -r Chromium -t 3
+wmctrl -r 'Microsoft​ Edge' -t 4
+wmctrl -r Postman  -t 6
+wmctrl -r Firefox -t 7
+
+
+
+
